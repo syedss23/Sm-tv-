5e7f1e63a22eadc211bbb9a0fe844c72cea509b4
syedss23 <kingabc139@gmail.com>
2025-10-12 19:14:25 +0530

    Update series.js

--- series.js
+++ series.js
@@ -194,10 +194,7 @@
             const tutorialVideo = `
               <section class="pro-video-card">
                 <div class="pro-video-frame-wrap">
-                  <iframe class='rumble'
-                    src='https://rumble.com/embed/v6x1664/?pub=2kaom2'
-                    allowfullscreen></iframe>
-                </div>
+                 <div style='position:relative;width:100%;height:0;padding-bottom:56.25%;'><iframe id='js_video_iframe' src='https://jumpshare.com/embed/k1uqX8VA5xpQQrcMC6q7' style='position:absolute;top:0;left:0;width:100%;height:100%;' frameborder='0' webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div>
               </section>
             `;
             document.getElementById('pro-episodes-row-wrap').innerHTML = html + tutorialTitle + tutorialVideo;
