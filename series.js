5ff4e648430eb3101b3e5f87b24c2bd0e2f425e5
syedss23 <kingabc139@gmail.com>
2025-10-20 17:55:26 +0530

    Update tutorial video iframe source URL

--- series.js
+++ series.js
@@ -194,7 +194,7 @@
             const tutorialVideo = `
               <section class="pro-video-card">
                 <div class="pro-video-frame-wrap">
-                 <div style='position:relative;width:100%;height:0;padding-bottom:56.25%;'><iframe class='rumble' src='https://rumble.com/embed/v6y0pdg/?pub=4ngog8' style='position:absolute;top:0;left:0;width:100%;height:100%;' frameborder='0' allowfullscreen></iframe></div>
+                 <div style='position:relative;width:100%;height:0;padding-bottom:56.25%;'><iframe class='rumble' src='https://rumble.com/embed/v6yd16u/?pub=4ni0h4' style='position:absolute;top:0;left:0;width:100%;height:100%;' frameborder='0' allowfullscreen></iframe></div>
               </section>
             `;
             document.getElementById('pro-episodes-row-wrap').innerHTML = html + tutorialTitle + tutorialVideo;
